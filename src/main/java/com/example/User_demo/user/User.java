552d0bf51f731acbddbec047f3e97dc7d71afabe
muhammed sule dabban <muhammedsulai4@gmail.com>
2025-12-31 15:26:27 +0100

    first commit

---
+++ src/main/java/com/example/User_demo/user/User.java
@@ -0,0 +1,23 @@
+package com.example.User_demo.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "_user")
+public class User {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+}
