926b71a1a092ba2ad474028feda1ffd4ca9204bf
muhammed sule dabban <muhammedsulai4@gmail.com>
2026-01-04 15:17:58 +0100

    today update

--- src/main/java/com/example/User_demo/config/ApplicationConfig.java
+++ src/main/java/com/example/User_demo/config/ApplicationConfig.java
@@ -4,10 +4,11 @@ import com.example.User_demo.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Component;
+
 
 @Configuration
 @RequiredArgsConstructor
@@ -15,9 +16,20 @@ public class ApplicationConfig {
 
     private final UserRepository repository;
 
+
     @Bean
     public UserDetailsService userDetailsService(){
         return username -> repository.findByEmail(username).orElseThrow(()-> new UsernameNotFoundException("user not found"));
 
+    }
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder()); // Use setPasswordEncoder
+        return authProvider;
+    }
+
+    private Object passwordEncoder() {
     }
 }
